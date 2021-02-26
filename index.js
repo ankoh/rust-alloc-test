55be6416ced933ef2fdd2e5df41bd5aace6ff369
Andre Kohn <kohn.a@outlook.com>
2021-02-26 14:15:21 +0100

    Text

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ console.log("small", small);
 const large = arrow_wasm.alloc2(1e9 / 4);
 console.log("large", large);
 console.log("small", small);
-console.log("small2", arrow_wasm.get1());
+console.log("small reload", arrow_wasm.get1());
